0676e8f9ab4d816b5129082b18e2bd8407d0053d
Дмитрий Виняр <dimavinyar@gmail.com>
2020-08-19 12:57:42 +0300

    - Выполнено упражнение 6.4 - Реорганизация осн. кода

--- Excercises.cpp
+++ Excercises.cpp
@@ -36,33 +36,97 @@ static void exercise_6_1()
 
 static void exercise_6_2()
 {
-    //ќбъ€вить 2 массива и добавить в них элементы в обратном пор€дке использу€ вложенный цикл
+    //ќбъ€вить 2 массива и сложить их элементы в обратном пор€дке использу€ вложенный цикл
     const int ARRAY1_LEN = 3;
     const int ARRAY2_LEN = 2;
 
-    int MyInts1[ARRAY1_LEN] = { 0 };
-    int MyInts2[ARRAY2_LEN] = { 0 };
+    int MyInts1[ARRAY1_LEN] = { 35, -3, 0 };
+    int MyInts2[ARRAY2_LEN] = { 20, -1 };
 
-    for (int ArrayIndex1 = 0; ArrayIndex1 < ARRAY1_LEN; ArrayIndex1++)
+    for (int ArrayIndex1 = ARRAY1_LEN-1; ArrayIndex1 >= 0; ArrayIndex1--)
     {
-        for (int ArrayIndex2 = 0; ArrayIndex2 < ARRAY2_LEN; ArrayIndex2++)
+        for (int ArrayIndex2 = ARRAY2_LEN-1; ArrayIndex2 >= 0; ArrayIndex2--)
         {
-            for (int ArrayResultIndex = ARRAY_RESULT_LEN - 1; ArrayResultIndex >= 0; ArrayResultIndex--)
-            {
-                Results[ArrayResultIndex] = MyInts1[ArrayIndex1] * MyInts2[ArrayIndex2];
-                std::cout << Results[(ARRAY_RESULT_LEN - 1) - ArrayResultIndex] << " ";
-            }
+            std::cout << "MyInts1[" << ArrayIndex1 << "] + MyInts2[" << ArrayIndex2 << "] = " << MyInts1[ArrayIndex1] + MyInts2[ArrayIndex2] << std::endl;
         }
     }
 }
 
 static void exercise_6_3()
 {
+    //¬ывести числа ‘ибоначчи в количестве, указанном пользователем
 
+    int NumsToCal = 0;
+    int Num1 = 0, Num2 = 1;
+
+    std::cout << "How many numbers do you want?" << std::endl;
+    std::cin >> NumsToCal;
+
+    std::cout << std::endl << Num1 << " " << Num2 << " ";
+
+    for (int Index = 0; Index < NumsToCal; Index++)
+    {
+        std::cout << Num1 + Num2 << " ";
+        Num2 += Num1;
+        Num1 = Num2 - Num1;
+    }
 }
 
 static void exercise_6_4()
 {
+    //Ќаписать конструкцию switch-case, провер€ющую, есть ли в радуге цвет
+
+    enum RainbowColor
+    {
+        Red = 0,
+        Pink,
+        White,
+        Orange,
+        Yellow,
+        Black,
+        Green,
+        Brown,
+        Cyan,
+        Blue,
+        Lime,
+        Purple
+    };
+
+    int UserColor = 0;
+
+    std::cout << "Available colors:" << std::endl;
+    std::cout << "Red = 0, Pink = 1, White = 2, Orange = 3, Yellow = 4," << std::endl << "Black = 5, Green = 6, Brown = 7,";
+    std::cout << "Cyan = 8, Blue = 9, Lime = 10, Purple = 11" << std::endl;
+
+    std::cout << "Enter color: ";
+    std::cin >> UserColor;
+
+    switch (UserColor)
+    {
+    case Red:
+        std::cout << "Rainbow has this color." << std::endl;
+        break;
+    case Orange:
+        std::cout << "Rainbow has this color." << std::endl;
+        break;
+    case Yellow:
+        std::cout << "Rainbow has this color." << std::endl;
+        break;
+    case Green:
+        std::cout << "Rainbow has this color." << std::endl;
+        break;
+    case Cyan:
+        std::cout << "Rainbow has this color." << std::endl;
+        break;
+    case Blue:
+        std::cout << "Rainbow has this color." << std::endl;
+        break;
+    case Purple:
+        std::cout << "Rainbow has this color." << std::endl;
+        break;
+    default:
+        std::cout << "No such color in a rainbow." << std::endl;
+    }
 
 }
 
--- Main.cpp
+++ Main.cpp
@@ -7,12 +7,21 @@ int main()
 
 	unsigned short ExerciseNumber = 0;
 	unsigned short ExerciseItem = 0;
+	
+	//Возврат при неправильном выборе
+	SelectExercise:
 
-	cout << "Run excercise No.: " << endl;
+	cout << "Run excercise No. (0 to quit): " << endl;
 	cin >> ExerciseNumber;
 
-	cout << "Run excercise " << ExerciseNumber << " item: " << endl;
+	if (ExerciseNumber == 0)
+		return 0;
+
+	cout << "Run excercise " << ExerciseNumber << " item (0 to quit): " << endl;
 	cin >> ExerciseItem;
+
+	if (ExerciseItem == 0)
+		return 0;
 
 	switch (ExerciseNumber)
 	{
@@ -20,25 +29,32 @@ int main()
 		exercise_5();
 		break;
 	case 6:
-		  switch (ExerciseItem)
-		  {
-		  case 1:
-			  exercise_6_1();
-			  break;
-		  case 2:
-			  exercise_6_2();
-			  break;
-		  case 3:
-			  exercise_6_3();
-			  break;
-		  case 4:
-			  exercise_6_4();
-			  break;
-		  case 5:
-			  exercise_6_5();
-			  break;
-		  }
-		  break;
+		switch (ExerciseItem)
+			{
+		case 1:
+			exercise_6_1();
+			break;
+		case 2:
+			exercise_6_2();
+			break;
+		case 3:
+			exercise_6_3();
+			break;
+		case 4:
+			exercise_6_4();
+			break;
+		case 5:
+			exercise_6_5();
+			break;
+		default:
+			cout << "Wrong input, try again" << endl;
+			goto SelectExercise;
+			}
+		break;
+	default:
+		cout << "Wrong input, try again" << endl;
+		goto SelectExercise;
 	}
+
 	return 0;
 }
